050e254d9e773be53b9a40949c3f9e04322a4b3c
Pedro Duarte <pedro.duarte@gerencianet.com.br>
2021-03-07 18:50:05 -0300

    initial: Initial: inicia o projeto!

---
+++ next.config.js
@@ -0,0 +1,15 @@
+const withImages = require('next-images')
+module.exports = withImages({
+  esModule: true,
+})
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+    enabled: process.env.ANALYZE === 'true',
+})
+module.exports = withBundleAnalyzer({})
+
+// next.config.js
+const withCSS = require('@zeit/next-css')
+module.exports = withCSS({
+  /* config options here */
+})
